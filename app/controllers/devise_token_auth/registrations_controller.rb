3233693f26a505ebfbf6e5eebc4190b37c14bca4
Andrea Reginato <andrea.reginato@gmail.com>
2015-05-12 17:12:24 +0200

    Removed email confirmation

--- app/controllers/devise_token_auth/registrations_controller.rb
+++ app/controllers/devise_token_auth/registrations_controller.rb
@@ -10,41 +10,42 @@ module DeviseTokenAuth
       @resource.uid        = sign_up_params[:email]
       @resource.provider   = "email"
 
-      # success redirect url is required
-      unless params[:confirm_success_url]
-        return render json: {
-          status: 'error',
-          data:   @resource,
-          errors: ["Missing `confirm_success_url` param."]
-        }, status: 403
-      end
+      ## success redirect url is required
+      #unless params[:confirm_success_url]
+        #return render json: {
+          #status: 'error',
+          #data:   @resource,
+          #errors: ["Missing `confirm_success_url` param."]
+        #}, status: 403
+      #end
 
       begin
         # override email confirmation, must be sent manually from ctrl
-        User.skip_callback("create", :after, :send_on_create_confirmation_instructions)
+        @resource.class.skip_callback("create", :after, :send_on_create_confirmation_instructions)
         if @resource.save
 
-          unless @resource.confirmed?
-            # user will require email authentication
-            @resource.send_confirmation_instructions({
-              client_config: params[:config_name],
-              redirect_url: params[:confirm_success_url]
-            })
+          #unless @resource.confirmed?
+            ## user will require email authentication
+            #@resource.send_confirmation_instructions({
+              #client_config: params[:config_name],
+              #redirect_url: params[:confirm_success_url]
+            #})
 
-          else
-            # email auth has been bypassed, authenticate user
-            @client_id = SecureRandom.urlsafe_base64(nil, false)
-            @token     = SecureRandom.urlsafe_base64(nil, false)
+          #else
 
-            @resource.tokens[@client_id] = {
-              token: BCrypt::Password.create(@token),
-              expiry: (Time.now + DeviseTokenAuth.token_lifespan).to_i
-            }
+          # email auth has been bypassed, authenticate user
+          @client_id = SecureRandom.urlsafe_base64(nil, false)
+          @token     = SecureRandom.urlsafe_base64(nil, false)
 
-            @resource.save!
+          @resource.tokens[@client_id] = {
+            token: BCrypt::Password.create(@token),
+            expiry: (Time.now + DeviseTokenAuth.token_lifespan).to_i
+          }
 
-            update_auth_header
-          end
+          @resource.save!
+
+          update_auth_header
+          #end
 
           render json: {
             status: 'success',
